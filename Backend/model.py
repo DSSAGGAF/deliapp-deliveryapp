34bcea08e0cb4daa331d4f3cd51761d25e56e7db
Abuda7m <d7oom.m.ssaggaf@gmail.com>
2020-11-21 21:46:52 +0800

    git ignore

--- Backend/model.py
+++ Backend/model.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.globals import request
 from marshmallow import Schema, fields, pre_load, validate
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
@@ -9,15 +10,16 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
 
-    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(), unique=True)
     firstname = db.Column(db.String())
     lastname = db.Column(db.String())
     password = db.Column(db.String())
     emailadress = db.Column(db.String())
     gender = db.Column(db.String())
+    driver_mode = db.Column(db.Boolean, default=False)
 
     def __init__(self, firstname, lastname, emailadress, password, username, gender):
         self.firstname = firstname
@@ -28,15 +30,85 @@ class User(db.Model):
         self.gender = gender
 
     def __repr__(self):
-        return '<id {}>'.format(self.id)
+        return '<user_id {}>'.format(self.user_id), '<driver_mode {}>'.format(self.driver_mode)
 
     def serialize(self):
         return {
-            'id': self.id,
+            'user_id': self.user_id,
             'username': self.username,
             'firstname': self.firstname,
             'lastname': self.lastname,
             'password': self.password,
             'emailadress': self.emailadress,
             'gender': self.gender,
+            'driver_mode': self.driver_mode,
+        }
+
+
+class Requset_Order(db.Model):
+    __tablename__ = 'requset_order'
+
+    request_id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.user_id'))
+    request_title = db.Column(db.String())
+    request_desc = db.Column(db.String())
+    request_from = db.Column(db.String())
+    request_to = db.Column(db.String())
+    price = db.Column(db.Integer())
+
+    def __init__(self, user_id, request_title, request_desc, request_from, request_to, price):
+        self.user_id = user_id
+        self.request_title = request_title
+        self.request_desc = request_desc
+        self.request_from = request_from
+        self.request_to = request_to
+        self.price = price
+
+    def __repr__(self):
+        return '<request_id {}>'.format(self.request_id)
+
+    def serialize(self):
+        return {
+            'request_id': self.request_id,
+            'user_id': self.user_id,
+            'request_title': self.request_title,
+            'request_desc': self.request_desc,
+            'request_from': self.request_from,
+            'request_to': self.request_to,
+            'price': self.price,
+
+        }
+
+class Requset_Order(db.Model):
+    __tablename__ = 'requset_order'
+
+    request_id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.user_id'))
+    request_title = db.Column(db.String())
+    request_desc = db.Column(db.String())
+    request_from = db.Column(db.String())
+    request_to = db.Column(db.String())
+    price = db.Column(db.Integer())
+
+    def __init__(self, user_id, request_title, request_desc, request_from, request_to, price):
+        self.user_id = user_id
+        self.request_title = request_title
+        self.request_desc = request_desc
+        self.request_from = request_from
+        self.request_to = request_to
+        self.price = price
+
+    def __repr__(self):
+        return '<request_id {}>'.format(self.request_id)
+
+    def serialize(self):
+        return {
+            'request_id': self.request_id,
+            'user_id': self.user_id,
+            'request_title': self.request_title,
+            'request_desc': self.request_desc,
+            'request_from': self.request_from,
+            'request_to': self.request_to,
+            'price': self.price,
+
         }
