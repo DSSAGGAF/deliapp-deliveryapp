89fcb9f310a032f52fc2534823b7248914bf933d
Abuda7m <d7oom.m.ssaggaf@gmail.com>
2021-01-28 18:40:47 +0800

    azoz version

--- Frontend/lib/network/api.dart
+++ Frontend/lib/network/api.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
 import 'dart:convert';
+// import 'dart:html';
 import 'package:http/http.dart' show Client;
 import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
 import "package:Deli_App/model/addUser.dart";
 import "package:Deli_App/model/orders.dart";
 import "package:Deli_App/model/notification.dart";
@@ -156,8 +160,22 @@ class API {
     }
   } //Order
 
+// Future<List<Order>> getAcceptedOrder() async {
+//   final response =
+//       await http.get('https://jsonplaceholder.typicode.com/albums/1');
+
+//   if (response.statusCode == 200) {
+//     // If the server did return a 200 OK response,
+//     // then parse the JSON.
+//     return Album.fromJson(jsonDecode(response.body));
+//   } else {
+//     // If the server did not return a 200 OK response,
+//     // then throw an exception.
+//     throw Exception('Failed to load album');
+//   }
+// }
   Future<List<Order>> getAcceptedOrder() async {
-    final Response response = await get('http://10.0.2.2:5000/api/myOrderUser?user_id=${userInfo.id}',
+    final Response response = await http.get('http://10.0.2.2:5000/api/myOrderUser?user_id=${userInfo.id}',
         headers: <String, String>{
           'Content-Type': 'application/json;charset=UTF-8'
         },
