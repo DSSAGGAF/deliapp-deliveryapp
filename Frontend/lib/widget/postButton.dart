9508d036f22fd0bd6f02046c17e163c4eeb741fd
Abuda7m <d7oom.m.ssaggaf@gmail.com>
2021-01-30 00:22:14 +0800

    fixing post button

--- Frontend/lib/widget/postButton.dart
+++ Frontend/lib/widget/postButton.dart
@@ -3,6 +3,7 @@ import "package:Deli_App/network/repository.dart";
 import 'package:Deli_App/widget/titleDesc.dart';
 import 'package:Deli_App/widget/map.dart';
 import 'package:Deli_App/widget/mapTo.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class PostButton extends StatefulWidget {
   @override
@@ -20,7 +21,12 @@ class _PostButtonState extends State<PostButton> {
         child: RaisedButton(
           onPressed: () {
             _repository.postRequest(
-                priceText.round(), titleController.text, descriptionController.text, addressData.addressLine, addressData2.addressLine);
+                priceText.round(),
+                titleController.text,
+                descriptionController.text,
+                addressData.addressLine,
+                addressData2.addressLine);
+            EasyLoading.showSuccess('Great Success!');
           },
           child: const Text('Post', style: TextStyle(fontSize: 20)),
           color: Colors.blue,
